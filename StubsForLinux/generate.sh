a1182c5651cd5f6515be94b10bf88d3c85f97df3
Sebastian Urban <surban@surban.net>
2018-05-03 22:58:09 +0200

    Updated stubs

--- StubsForLinux/generate.sh
+++ StubsForLinux/generate.sh
@@ -19,8 +19,9 @@ genstub ()
 }
 
 
-# nvcuda.dll or libcuda.so.1 is always installed by the GPU driver.
+# nvcuda.dll or libcuda.so.1 and nvml.dll or libnvidia-ml.so.1 are always installed by the GPU driver.
 genstub nvcuda libcuda.so.1
+genstub nvml libnvidia-ml.so.1
 
 # These libraries are from the CUDA SDK and redistributed in the NuGet packages.
 genstub cublas64_91 libcublas.so.9.1
@@ -30,10 +31,19 @@ genstub cusolver64_91 libcusolver.so.9.1
 genstub cusparse64_91 libcusparse.so.9.1
 genstub nvgraph64_91 libnvgraph.so.9.1
 genstub nvrtc64_91 libnvrtc.so.9.1
+genstub nppc64_91 libnppc.so.9.1
+genstub nppial64_91 libnppial.so.9.1
+genstub nppicc64_91 libnppicc.so.9.1
+genstub nppicom64_91 libnppicom.so.9.1
+genstub nppidei64_91 libnppidei.so.9.1
+genstub nppif64_91 libnppif.so.9.1
+genstub nppig64_91 libnppig.so.9.1
+genstub nppim64_91 libnppim.so.9.1
+genstub nppist64_91 libnppist.so.9.1
+genstub nppisu64_91 libnppisu.so.9.1
+genstub nppitc64_91 libnppitc.so.9.1
+genstub npps64_91 libnpps.so.9.1
 
-# cuDNN redistribution is prohibited.
+# cuDNN redistribution is prohibited, but stub is generated to map DLL name.
 genstub cudnn64_7 libcudnn.so.7.0
 
-#genstub nppi64_80 libnppi.so.8.0
-#genstub nppc64_80 libnppc.so.8.0
-#genstub npps64_80 libnpps.so.8.0
