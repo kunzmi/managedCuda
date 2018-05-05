501fc04f60c47fe83ceaea72539fb11b25c6ab18
Sebastian Urban <surban@surban.net>
2018-05-05 14:59:58 +0200

    Cleanup

--- Scripts/ManageBuildNumbers.ps1
+++
@@ -1,9 +0,0 @@
-function Update-BuildVersionFromRevision
-{  
-    [CmdletBinding()]  
-    param()  
-          
-    $gitCommitDistance = git rev-list HEAD --count
-    Set-Content -Path "Revision.targets" -Value "<Project><PropertyGroup><Rev>$gitCommitDistance</Rev></PropertyGroup></Project>" 
-}
-
--- Scripts/NuGetBuild.ps1
+++
@@ -1,6 +0,0 @@
-. "$PSScriptRoot/ManageBuildNumbers.ps1"
-
-Push-Location "$PSScriptRoot/.."
-Update-BuildVersionFromRevision
-dotnet pack -c Release
-Pop-Location
